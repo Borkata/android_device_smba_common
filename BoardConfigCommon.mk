532af0ae627fe207af39f11bac588a615e8b7c10
Boris Popov <bvpopov@gmail.com>
2014-03-10 14:10:19 -0700

    BoardConfigCommon: Bring up 4.4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -24,18 +24,36 @@ TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
 #arhitecture
+TARGET_ARCH := arm
+TARGET_BOARD_PLATFORM := tegra
+TARGET_TEGRA_VERSION := t20
+TARGET_BOOTLOADER_BOARD_NAME := harmony
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_ARCH_VARIANT_FPU := vfpv3-d16
 TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := generic
+
+# Compiler Optimization - This is a @codefireX specific flag to use -O3 everywhere.
+ARCH_ARM_HIGH_OPTIMIZATION := true
+# ANDROID, LINUX-ARM AND TLS REGISTER EMULATION
 ARCH_ARM_HAVE_TLS_REGISTER := true
+# Avoid the generation of ldrcc instructions
+NEED_WORKAROUND_CORTEX_A9_745320 := true
+#define to use all of the Linaro Cortex-A9 optimized string funcs,
+#instead of subset known to work on all machines
+USE_ALL_OPTIMIZED_STRING_FUNCS := true
+# customize the malloced address to be 16-byte aligned
+BOARD_MALLOC_ALIGNMENT := 16
+TARGET_EXTRA_CFLAGS := $(call cc-option,-mtune=cortex-a9) $(call cc-option,-mcpu=cortex-a9)
 
 PRODUCT_CHARACTERISTICS := tablet
 BOARD_USE_SKIA_LCDTEXT := true
 SMALLER_FONT_FOOTPRINT := true
+BOARD_NO_ALLOW_DEQUEUE_CURRENT_BUFFER := true
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
 
 #platform
 TARGET_NO_RADIOIMAGE := true
@@ -47,6 +65,9 @@ BOARD_EGL_NEEDS_LEGACY_FB := false
 BOARD_USES_GENERIC_AUDIO := true
 BOARD_USES_AUDIO_LEGACY := false
 BOARD_USES_ALSA_AUDIO := false
+BOARD_OMX_NEEDS_LEGACY_AUDIO := true
+
+BOARD_USES_GENERIC_INVENSENSE := false
 
 #bluetooth
 BOARD_HAVE_BLUETOOTH := true
@@ -58,6 +79,24 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/malata/smba_common/bluetoo
 BOARD_EGL_CFG := device/malata/smba_common/prebuilt/egl.cfg
 BOARD_USES_OVERLAY := true
 USE_OPENGL_RENDERER := true
+BOARD_USES_HGL := true
+BOARD_USE_MHEAP_SCREENSHOT := true
+BOARD_EGL_SKIP_FIRST_DEQUEUE := true
+BOARD_USES_HWCOMPOSER := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+SKIP_SET_METADATA := true
+
+# Enable WEBGL in WebKit
+ENABLE_WEBGL := true
+
+# Sensors
+TARGET_USES_OLD_LIBSENSORS_HAL := false
+
+# Compatibility with pre-kitkat Sensor HALs
+SENSORS_NEED_SETRATE_ON_ENABLE := true
+
+# Misc flags
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 
 # Wifi related defines
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
@@ -70,6 +109,10 @@ WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/system/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_P2P     := "/system/vendor/firmware/fw_bcmdhd_p2p.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
+
+# Wi-Fi AP
+BOARD_LEGACY_NL80211_STA_EVENTS := true
+BOARD_NO_APSME_ATTR := true
  
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_PAGE_SIZE := 0x00000800
@@ -78,3 +121,49 @@ BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000
 BOARD_USES_SECURE_SERVICES := true
 
 TARGET_OTA_ASSERT_SKIP := true
+
+# Preload bootanimation in to memory
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+TARGET_BOOTANIMATION_USE_RGB565 := true
+TARGET_SCREEN_WIDTH := 1024
+TARGET_SCREEN_HEIGHT := 600
+
+# Override healthd HAL
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.harmony
+
+# SELinux policies
+HAVE_SELINUX := true
+
+ifeq ($(HAVE_SELINUX),true)
+
+	#POLICYVERS := 24
+
+	BOARD_SEPOLICY_DIRS += \
+	device/notionink/adam_common/sepolicy
+ 
+BOARD_SEPOLICY_UNION := \
+    file_contexts \
+    app.te \
+    device.te \
+    drmserver.te \
+    file.te \
+    genfs_contexts \
+    healthd.te \
+    init.te \
+    media_app.te \
+    release_app.te \
+    mediaserver.te \
+    platform_app.te \
+    sensors_config.te \
+    shared_app.te \
+    surfaceflinger.te \
+    system_app.te \
+    system.te \
+    untrusted_app.te \
+    vold.te \
+    wpa_socket.te \
+    wpa.te \
+    zygote.te
+
+endif
